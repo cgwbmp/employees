4a14e21b4ad50117938dba1938b2aae40f553370
Alena <cgwbmp@gmail.com>
2019-10-30 20:55:32 +0500

    Updates

---
+++ precache-manifest.f649fedac7f95bc6992ceb776770ab6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6214dae348f455d3ac0a23b4d2bda04",
+    "url": "/employees/index.html"
+  },
+  {
+    "revision": "790029a547803fb33a77",
+    "url": "/employees/static/css/2.d2f85bed.chunk.css"
+  },
+  {
+    "revision": "62de6ac376a0caaa56eb",
+    "url": "/employees/static/css/main.3ec6fde4.chunk.css"
+  },
+  {
+    "revision": "790029a547803fb33a77",
+    "url": "/employees/static/js/2.15b16399.chunk.js"
+  },
+  {
+    "revision": "62de6ac376a0caaa56eb",
+    "url": "/employees/static/js/main.519d6164.chunk.js"
+  },
+  {
+    "revision": "1633baa542a1517a63da",
+    "url": "/employees/static/js/runtime-main.1b495ca0.js"
+  }
+]);
